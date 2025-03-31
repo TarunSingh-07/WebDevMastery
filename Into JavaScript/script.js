52f355418c8c6832b8465ffe7bb1175c56732948
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-03-31 20:18:33 +0530

    Add interactive console logs and prompts to script.js for user engagement

--- Into JavaScript/script.js
+++ Into JavaScript/script.js
@@ -1,1 +1,17 @@
 alert("Hello World");
+
+console.log("Code is running...");
+console.log("Code is also running....");
+console.log("Code is looking like a wow.....");
+
+var a = prompt("Enter your number:")
+console.log("Your number is " + a)
+
+var isTrue = confirm("Are you ready to blast 🔥 your Computer")
+
+if (isTrue) {
+    console.log("Computer is Blasting 🔥")
+} else {
+    console.log("Computer is not Blasting 🤖")
+}
+
