a2883a6424a4ada52bd099c1fc3ea031ac86928e
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-02 00:39:15 +0530

    Fix variable declaration issues and add comments in script.js for clarity

--- Js Var Dt Obj/script.js
+++ Js Var Dt Obj/script.js
@@ -3,6 +3,12 @@ console.log("This is about Variables and Data Types in JavaScript")
 var a = 10
 var b = 20
 var c = "Tarun"
+var_a = "Alex"
+// var 55a = "Ram"
 
 console.log(a + b + 8)
 console.log(typeof a, typeof b, typeof c)
+
+// const canot be changed after declaration
+const a1 = 6
+a1 = a1+ 1
