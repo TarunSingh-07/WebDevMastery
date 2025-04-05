98942136bae3c0636dd010486213386bc4c2e820
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-05 21:29:13 +0530

    Comment out unused object and loop examples in index.js

--- Js Loops/index.js
+++ Js Loops/index.js
@@ -9,18 +9,31 @@ let a = 1
 //     console.log(a + i)
 // }
 
-let obj = {
-    name: "Tarun",
-    role: "Developer",
-    company: "Equinox"
-}
 
-for (const key in obj) {
-    const element = obj[key]
-    console.log(key, element)
-}
+// let obj = {
+//     name: "Tarun",
+//     role: "Developer",
+//     company: "Equinox"
+// }
 
-for (const c of "Tarun") {
-    console.log(c)
-}
+// for (const key in obj) {
+//     const element = obj[key]
+//     console.log(key, element)
+// }
 
+// for (const c of "Tarun") {
+//     console.log(c)
+// }
+
+
+// i = 0
+// while (i<6) {
+//     console.log(i)
+//     i++
+// }
+
+let i = 10
+do {
+    console.log(i)
+    i++
+}while (i<6)
