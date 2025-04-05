86208ea84f99241cfec2bd434c68689d63d6af8f
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-05 21:21:00 +0530

    Refactor index.js to include object iteration and character logging examples

--- Js Loops/index.js
+++ Js Loops/index.js
@@ -1,1 +1,26 @@
 console.log("I am about Loops")
+
+let a = 1
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i)
+// }
+
+let obj = {
+    name: "Tarun",
+    role: "Developer",
+    company: "Equinox"
+}
+
+for (const key in obj) {
+    const element = obj[key]
+    console.log(key, element)
+}
+
+for (const c of "Tarun") {
+    console.log(c)
+}
+
