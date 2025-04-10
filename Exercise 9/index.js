94f6eec3e28d3bf8e1e9fb5d9a173945a3bc57ca
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-10 21:02:25 +0530

    Implement faulty calculator logic and add HTML structure

--- Exercise 9/index.js
+++ Exercise 9/index.js
@@ -15,6 +15,31 @@ It performs wrong operation 10% of the times
 
 console.log("Faulty Calculator")
 
+let random = Math.random()
+console.log(random)
+let a = prompt("Enter first number")
+let c = prompt("Enter Operation")
+let b = prompt("Enter Second number")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**"
+}
+
+if (random > 0.1) {
+    // Perform the correct operation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+
+} else {
+    // Perform the wrong operation
+    c = obj[c]
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
 function faultyCalculator(num1, num2) {
 
 }
