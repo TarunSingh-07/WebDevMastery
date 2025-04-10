9f0bdc246aa6c3f16fd51bc634ecf5825940c9fd
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-10 19:21:53 +0530

    Add faulty calculator description in index.js

---
+++ Exercise 9/index.js
@@ -0,0 +1,14 @@
+/* Create a Faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from user
+2. It performs wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+It performs wrong operation 10% of the times
+
+*/
