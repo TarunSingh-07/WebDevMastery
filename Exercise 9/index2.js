85936cba2807eb5172b73e6d633908ead4cbd134
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-10 22:19:53 +0530

    Add faulty calculator implementation and update HTML script reference

---
+++ Exercise 9/index2.js
@@ -0,0 +1,56 @@
+// Faulty calculater
+console.log("Faulty Calculator")
+
+let random = Math.random()
+console.log(random)
+
+let a = Number(prompt("Enter first number"))
+let c = prompt("Enter Operation")
+let b = Number(prompt("Enter Second number"))
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**"
+}
+let result
+if (random > 0.1){
+    if (c == "+"){
+        result = a + b
+    }
+    else if (c == "-" ){
+        result = a - b
+    }
+    else if (c == "*" ){
+        result = a * b
+    }
+    else if (c == "/" ){
+        result = a / b
+    }
+
+} else {
+    c = obj[c]
+    if (c == "+"){
+        result = a - b
+    }
+    else if (c == "-" ){
+        result = a / b
+    }
+    else if (c == "*" ){
+        result = a + b
+    }
+    else if (c == "/" ){
+        result = a ** b
+    }
+
+}
+
+// In without obj creation we use the === instead of == because == is used for type conversion and === is used for strict comparison
+
+console.log(`The result is ${a} ${c} ${b} : `, result)
+alert(`The result is ${result}`)
+
+// console.log("The result is " + result)
+// alert("The result is " + result)
+
