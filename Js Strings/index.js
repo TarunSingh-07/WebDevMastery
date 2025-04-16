c5ae4919fb21f19c561d06b42d57c9f450b4a221
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-16 16:21:49 +0530

    Add string tutorial example in index.js

---
+++ Js Strings/index.js
@@ -0,0 +1,14 @@
+console.log("This is String Tutorial")
+let a = "Tarun"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5]) // undefined
+
+console.log(a.length)
+
+let real_name = "Tarun"
+let friend = "Reeny"
+console.log("His name is " + real_name + " and his friend name is " + friend)
