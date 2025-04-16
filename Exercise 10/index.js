b1fc2d937d2a78e46087b68d6441f3aabcc339a7
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-17 01:05:26 +0530

    Refactor business name generator comments and structure in index.js

--- Exercise 10/index.js
+++ Exercise 10/index.js
@@ -1,2 +1,36 @@
-// Create a business name generator by combining list of adjectives and shop name and another word
+/* Create a business name generator by combining list of adjectives and shop name and another word
 
+Adjectives :
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+*/
+
+console.log("Business Name Generator")
+
+let adj1 = "Crazy"
+let adj2 = "Amazing"
+let adj3 = "Fire"
+
+let shopName1 = "Engine"
+let shopName2 = "Foods"
+let shopName3 = "Garments"
+
+let anotherWord1 = "Bros"
+let anotherWord2 = "Limited"
+let anotherWord3 = "Hub"
+
+function generateBusinessName() {
+    let randomAdj = 
+}
