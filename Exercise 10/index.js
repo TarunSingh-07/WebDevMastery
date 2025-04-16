3dc475af37e69caccb6476644a85a0d50a518351
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-17 01:43:49 +0530

    Enhance business name generator output with additional console messages

--- Exercise 10/index.js
+++ Exercise 10/index.js
@@ -17,7 +17,8 @@ Hub
 
 */
 
-console.log("Business Name Generator")
+console.log("Business Name Generator : ")
+console.log("🎉 Get ready for an awesome business name! 🎉");
 
 let adj1 = "Crazy"
 let adj2 = "Amazing"
@@ -32,5 +33,40 @@ let anotherWord2 = "Limited"
 let anotherWord3 = "Hub"
 
 function generateBusinessName() {
-    let randomAdj = 
+    let randomAdj = Math.floor(Math.random() * 3)
+    let Adjective
+    if (randomAdj === 0){
+        Adjective = adj1
+    }else if (randomAdj === 1){
+        Adjective = adj2
+    }else {
+        Adjective = adj3
+    }
+
+    let randomShopName = Math.floor(Math.random() * 3)
+    let ShopName
+    if (randomShopName === 0){
+        ShopName = shopName1
+    }
+    else if (randomShopName === 1){
+        ShopName = shopName2
+    }
+    else{
+        ShopName = shopName3
+    }
+    
+    let randomAnotherWord = Math.floor(Math.random() * 3)
+    let AnotherWord
+    if (randomAnotherWord === 0){
+        AnotherWord = anotherWord1
+    }else if (randomAnotherWord === 1){
+        AnotherWord = anotherWord2
+    }else {
+        AnotherWord = anotherWord3
+    }
+    
+    let businessName = `${Adjective} ${ShopName} ${AnotherWord}`
+    console.log(businessName)
 }
+
+generateBusinessName()
