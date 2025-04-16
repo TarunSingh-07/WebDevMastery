34b3ff95321efbfd155181b6046c9fd1338581a8
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-16 16:51:06 +0530

    Add string manipulation examples to index.js

--- Js Strings/index.js
+++ Js Strings/index.js
@@ -12,3 +12,15 @@ console.log(a.length)
 let real_name = "Tarun"
 let friend = "Reeny"
 console.log("His name is " + real_name + " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`) // Template literals
+
+let b = "Shivam"
+console.log(b.toUpperCase()) 
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 4)) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))  // replace first occurrence
+console.log(b.concat(a, "Rahul", "Equip")) // concat
