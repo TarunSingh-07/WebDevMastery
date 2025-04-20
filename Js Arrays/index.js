75b57261edc902529990ab233746aef3c35a6383
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-20 21:02:52 +0530

    Add array manipulation examples in index.js

---
+++ Js Arrays/index.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 5 ,7, 8]
+// Index   0, 1, 2, 3, 4
+
+// Strings are immutable and Arrays are mutable
+
+arr[0] = 2666
+console.log(arr)
+console.log(arr.length)
+
+
+arr[0] = 2666
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
+
