4add91ca05bf90c0df3881934df0216b271899af
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-24 01:14:39 +0530

    Add factorial calculation function and update console output in for.js

--- Exercise 11/for.js
+++ Exercise 11/for.js
@@ -6,3 +6,19 @@ for (let i = 1; i <= a; i++) {
 
 }
 console.log("The factorial is : ", factorial) // 720
+
+
+// OR
+// Using Function
+
+function factorialFunction(n) {
+    let factorial = 1
+    for (let i = 1; i <= n; i++) {
+        factorial *= i
+    }
+    return factorial
+}
+
+let number = Number(prompt("Enter a number"))
+console.log(`The factorial of ${number} is ${factorialFunction(number)}`)
+
