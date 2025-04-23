4d9867a13b2934b26d4907f1c1434060234a2f53
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-23 20:10:38 +0530

    Fix formatting issues in mfr.js by adding spaces for readability in filter and reduce functions

--- Js Arrays/mfr.js
+++ Js Arrays/mfr.js
@@ -21,17 +21,17 @@ console.log(newArr) // [1, 169, 25, 49, 121]
 // Filer method is used to create a new array by filtering the elements of the original array based on a condition.
 
 const greaterThanSeven = (e) => {
-    if(e>7){
+    if (e > 7) {
         return true
     }
     return false
 }
-console.log(arr.filter(greaterThanSeven)) 
+console.log(arr.filter(greaterThanSeven))
 
 // reduce function is used to reduce the array to a single value by applying a function to each element of the array.
 let arr2 = [1, 2, 3, 4, 5, 6]
 const red = (a, b) => {
-    return a*b
+    return a * b
 }
 console.log(arr2.reduce(red)) // 720 --  1*2*3*4*5*6 = 720
 
