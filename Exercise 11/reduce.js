706728219ef1b6c2e93565bf4bb0cc0594c7b606
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-04-24 02:00:16 +0530

    Refactor factorial function to include complete array and improve console output

--- Exercise 11/reduce.js
+++ Exercise 11/reduce.js
@@ -1,8 +1,19 @@
 let a = 6
 
 function factorial(number) {
-    let arr = Array.from(Array(number).keys())
-    console.log(arr)
-
+    let arr = Array.from(Array(number+1).keys()) // [ 0, 1, 2, 3, 4, 5, 6 ]
+    console.log(arr.slice(1,)) // [ 1, 2, 3, 4, 5, 6 ]
+    let c = arr.slice(1,).reduce((a, b)=> { 
+        return a * b
+    })
+    console.log(c) // 720
 }
 factorial(a)
+
+
+
+
+/* reminder
+let arr = Array.from(Array(number).keys()) // [ 0, 1, 2, 3, 4, 5 ]
+console.log(arr) // [ 0, 1, 2, 3, 4, 5 ]
+*/
