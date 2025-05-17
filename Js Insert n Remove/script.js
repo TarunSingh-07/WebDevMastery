d49c292935283e824807bfdb4fbe456e2e9ac0aa
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-05-17 21:34:15 +0530

    Refactor script in index.html for clarity and update comments in script.js

--- Js Insert n Remove/script.js
+++ Js Insert n Remove/script.js
@@ -1,24 +1,24 @@
-document.querySelector(".container").innerText
-//'Hey I am a box\nHey I am a box'
+// document.querySelector(".container").innerText
+// //'Hey I am a box\nHey I am a box'
 
-document.querySelector(".container").outer
-//undefined
+// document.querySelector(".container").outer
+// //undefined
 
-document.querySelector(".container").outerHTML
-//'<div class="container">\n        <div class="box">Hey I am a box</div>\n        <div class="box">Hey I am a box</div>\n    </div>'
+// document.querySelector(".container").outerHTML
+// //'<div class="container">\n        <div class="box">Hey I am a box</div>\n        <div class="box">Hey I am a box</div>\n    </div>'
 
-document.querySelector(".container").tagName
-//'DIV'
+// document.querySelector(".container").tagName
+// //'DIV'
 
-document.querySelector(".container").nodeName
-//'DIV'
+// document.querySelector(".container").nodeName
+// //'DIV'
 
-document.querySelector(".container").textContent
-// '\n        Hey I am a box\n        Hey I am a box\n    '
-document.querySelector(".container").hidden
-//false
+// document.querySelector(".container").textContent
+// // '\n        Hey I am a box\n        Hey I am a box\n    '
+// document.querySelector(".container").hidden
+// //false
 
-document.querySelector(".container").hidden = true
+// document.querySelector(".container").hidden = true
 
-document.querySelector(".container").innerHTML = "Hey I am Tarun"
-//'Hey I am Tarun'
+// document.querySelector(".container").innerHTML = "Hey I am Tarun"
+// //'Hey I am Tarun'
