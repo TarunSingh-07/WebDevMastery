384ed9eec536966d92f06555513f070f68282c00
TarunSingh-07 <tarunsinghtech@gmail.com>
2025-05-21 13:53:38 +0530

    m,,m

--- Exercise 13/script.js
+++ Exercise 13/script.js
@@ -24,5 +24,6 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     document.querySelector(".container").innerHTML += html
 }
 
+
 createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry",
     560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
