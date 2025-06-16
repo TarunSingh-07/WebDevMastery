791602b07c7e1a2da98f263621bd06040d9f468d
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-06-17 02:16:05 +0530

    Refactor promise.js to improve promise handling and add error logging

--- Js Callbacks nd Promises/promise.js
+++ Js Callbacks nd Promises/promise.js
@@ -2,5 +2,23 @@ console.log('This is promises');
 
 
 let prom1 = new Promise((resolve, reject) => {
-    
+    let a = Math.random()
+    console.log(a);
+
+    if (a < 0.5) {
+        reject("The random number is less than 0.5")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes i am done");
+            resolve("Tarun")
+        }, 3000);
+    }
 })
+
+prom1.then((result) => {
+    console.log(result);
+})
+    .catch((error) => {
+        console.log(error);
+    })
