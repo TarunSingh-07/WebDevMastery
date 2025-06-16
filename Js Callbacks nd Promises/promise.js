55a37a20bd09a0a799612b019b0d28b3e36f6ccd
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-06-17 02:47:00 +0530

    Refactor promise.js to clean up commented code and adjust timeout duration

--- Js Callbacks nd Promises/promise.js
+++ Js Callbacks nd Promises/promise.js
@@ -12,13 +12,40 @@ let prom1 = new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("Yes i am done");
             resolve("Tarun")
-        }, 3000);
+        }, 2000);
     }
 })
 
-prom1.then((result) => {
-    console.log(result);
+// prom1.then((result) => {
+//     console.log(result);
+// })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random()
+    console.log(a);
+    if (a < 0.5) {
+        reject("The random number is less than 0.5 againnnnn")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes i am done again");
+            resolve("Jinwoo")
+        }, 1000);
+    }
 })
-    .catch((error) => {
-        console.log(error);
-    })
+
+// let p3 = Promise.allSettled([prom1, prom2]) // Used to wait for all Promises to finish,no matter they reject or resolve and get their results.
+
+// let p3 = Promise.race([prom1, prom2]) // Used to get the result of the first Promise that resolves or rejects. 
+
+
+let p3 = Promise.all([prom1, prom2])
+p3.then(a=>{
+    console.log(a);
+}).catch(err=>{
+    console.log(err);
+})
