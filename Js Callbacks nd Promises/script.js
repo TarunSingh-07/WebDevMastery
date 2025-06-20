793e5b89544f3281c4386211b19feada92effc26
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-06-21 01:13:02 +0530

    Add examples for callback functions in script.js

--- Js Callbacks nd Promises/script.js
+++ Js Callbacks nd Promises/script.js
@@ -32,3 +32,33 @@ const loadScript = (src, callback) => {
 }
 
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
+
+// example 2
+
+function greet(name, callback) {
+    console.log("Hello, " + name);
+    callback();
+}
+
+function sayBye() {
+    console.log("Goodbye!");
+}
+
+greet("Ajay", sayBye);
+
+// example 3
+
+function calc(a, b, callback) {
+    return callback(a, b);
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function mul(x, y) {
+    return x * y;
+}
+
+console.log(calc(5, 3, add));    
+console.log(calc(5, 3, mul));
