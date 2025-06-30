ffeda296e8a3567e31cfad357d88b01e2f035fa1
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-06-30 18:31:18 +0530

    Add initial HTML and JavaScript files for async operations

---
+++ Js async n await/script.js
@@ -0,0 +1,41 @@
+function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(455)
+        }, 3500);
+    })
+
+}
+
+async function main(){
+
+    console.log('Loading modules');
+    
+    console.log('Do something else');
+    
+    console.log('Load data');
+    let data = await getData()
+    
+    console.log(data);
+    
+    console.log('process data');
+    console.log('task 2');
+}
+
+
+main()
+
+
+// data.then((v) => {
+
+//     console.log(data);
+
+//     console.log('process data');
+
+//     console.log('task 2');
+
+// })
+
+
+
+
