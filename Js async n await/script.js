8c1417f991000d448cd09a7dd04bfe131e1d8a9d
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-07-02 19:17:37 +0530

    Refactor getData function to remove commented-out code and improve clarity

--- Js async n await/script.js
+++ Js async n await/script.js
@@ -6,9 +6,25 @@
 //     })
 
 // }
+
+
+
 async function getData() {
     // simulate getting data from a server
-    let x  = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // let x  = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+
     let data = await x.json()
     return data
     // let data = await x.text()
@@ -22,6 +38,7 @@ async function main() {
     console.log('Do something else');
 
     console.log('Load data');
+
     let data = await getData()
 
     console.log(data);
