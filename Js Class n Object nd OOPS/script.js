e04e581883e41ed2354d3e63e98c013e3801d014
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-07-08 23:55:40 +0530

    Add index.html and script.js files for project setup and initial functionality

---
+++ Js Class n Object nd OOPS/script.js
@@ -0,0 +1,45 @@
+// let obj = {
+//     a: 1,
+//     b: "Tarun"
+// }
+// console.log(obj);
+
+// let animal = {
+//     eats: true
+// }
+// let rabbit = {
+//     jumps: true
+// }
+
+// rabbit.__proto__ = animal // sets rabbit.[[Prototype]] = animal
+
+
+class Animal {
+    constructor(name){
+        this.name = name
+        console.log('Object is created.....');
+    }
+    eats(){
+        console.log('Kha rha hu');
+    }
+    jumps(){
+        console.log('Kood raha hoon');
+    }
+}
+
+
+class Lion extends Animal{
+    constructor(name){
+        super(name)
+        console.log('Object is created and he is a lion...');
+    }
+    eats(){
+        console.log('Kha rha hu roarrrr');
+    }
+}
+
+let a = new Animal("Bunny")
+console.log(a);
+
+let l = new Lion('Shera')
+console.log(l);
